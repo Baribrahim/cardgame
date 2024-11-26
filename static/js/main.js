c05074e46ae762fe4587a59f95dc4ccb24898d79
Abdulbari <bari-02@hotmail.com>
2024-11-26 20:07:42 +0000

    Added styling

---
+++ static/js/main.js
@@ -0,0 +1,9 @@
+// main.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    const cardImage = document.getElementById('card-image');
+
+    if (cardImage) {
+        cardImage.classList.add('deal-animation');
+    }
+});
